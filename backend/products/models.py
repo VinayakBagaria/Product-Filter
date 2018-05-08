008c95c35a1c6c40481a77f4da9a85f85ae56b32
VinayakBagaria <workvinayak95@gmail.com>
2018-05-08 12:39:32 +0530

    model created for product

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    COLORS = (
+        ('Red', 'Red'),
+        ('Black', 'Black'),
+        ('Blue', 'Blue'),
+        ('Green', 'Green'),
+        ('Yellow', 'Yellow')
+    )
+    name = models.CharField(max_length=80)
+    rating = models.FloatField(
+        default=1.0,
+        validators=[MaxValueValidator(5.0), MinValueValidator(1.0)]
+    )
+    price = models.PositiveIntegerField()
+    discount = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(100)]
+    )
+    brand = models.CharField(max_length=80)
+    color = models.CharField(max_length=10, choices=COLORS)
