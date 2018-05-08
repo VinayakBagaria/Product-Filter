2ce735f985839e2b0011f2ef5cffd2991a1522f4
VinayakBagaria <workvinayak95@gmail.com>
2018-05-08 19:10:32 +0530

    edge cases for product fetching

--- frontend/src/app/components/Button.js
+++ frontend/src/app/components/Button.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+const Button = ({ text, onClick, disabled }) => (
+  <button onClick={onClick} disabled={disabled}>
+    {text}
+  </button>
+);
 
 Button.propTypes = {
   text: PropTypes.number.isRequired,
   onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
 };
 
 export default Button;
--- frontend/src/app/components/Container.js
+++ frontend/src/app/components/Container.js
@@ -7,16 +7,19 @@ import { fetchData, changePage } from './../../actions';
 
 class Container extends Component {
   componentDidMount() {
-    this.props.dispatch(fetchData(this.props.page));
+    this.props.dispatch(fetchData(this.props.pageNumber));
   }
   render() {
-    const { count, products, dispatch } = this.props;
+    const {
+      isFetching, pageNumber, count, products, dispatch,
+    } = this.props;
     const totalPages = count / products.length;
     const buttons = [];
     for (let i = 1; i <= totalPages; i += 1) {
       buttons.push(<Button
         key={i}
         text={i}
+        disabled={pageNumber === i}
         onClick={e => {
             e.preventDefault();
             dispatch(changePage(i));
@@ -25,9 +28,15 @@ class Container extends Component {
     }
     return (
       <Fragment>
-        {products.map(product => (
-          <Product key={product.pk} product={product} />
-        ))}
+        {isFetching && products.length === 0 && <h2>Loading...</h2>}
+        {!isFetching && products.length === 0 && <h2>Empty.</h2>}
+        {products.length > 0 && (
+          <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+            {products.map(product => (
+              <Product key={product.pk} product={product} />
+            ))}
+          </div>
+        )}
         {buttons}
       </Fragment>
     );
@@ -35,17 +44,21 @@ class Container extends Component {
 }
 
 function mapStateToProps(state) {
-  const { products, count } = state.result;
+  const {
+    isFetching, products, count, pageNumber,
+  } = state.result;
   return {
-    page: state.page,
+    isFetching,
+    pageNumber,
     products,
     count,
   };
 }
 
 Container.propTypes = {
+  isFetching: PropTypes.bool.isRequired,
   count: PropTypes.number.isRequired,
-  page: PropTypes.number.isRequired,
+  pageNumber: PropTypes.number.isRequired,
   products: PropTypes.arrayOf(PropTypes.shape({
     pk: PropTypes.number,
     name: PropTypes.string,
@@ -55,6 +68,10 @@ Container.propTypes = {
     color: PropTypes.arrayOf(PropTypes.string),
   })).isRequired,
   dispatch: PropTypes.func.isRequired,
+};
+
+Container.defaultProps = {
+  isFetching: false,
 };
 
 export default connect(mapStateToProps)(Container);
