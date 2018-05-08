f9325273c949c52a94308f6336dcff7f7e9c9a83
VinayakBagaria <workvinayak95@gmail.com>
2018-05-08 13:27:33 +0530

    simple pagination

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,13 +1,22 @@
 from django.db.models import Q
 from rest_framework import generics, mixins
 
+from rest_framework.pagination import PageNumberPagination
+
 from .models import Product
 from .serializers import ProductSerializer
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 10
 
 
 class AllProductsView(mixins.CreateModelMixin, generics.ListAPIView):
     lookup_field = 'pk'
     serializer_class = ProductSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         qs = Product.objects.all()
