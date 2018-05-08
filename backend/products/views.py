81921ba4a695d093d86239d33067e0c37bd8d112
VinayakBagaria <workvinayak95@gmail.com>
2018-05-08 13:23:13 +0530

    view for getting all products

--- backend/products/views.py
+++ backend/products/views.py
@@ -5,6 +5,26 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-class AllProductsView(generics.ListAPIView):
-    queryset = Product.objects.all()
+class AllProductsView(mixins.CreateModelMixin, generics.ListAPIView):
+    lookup_field = 'pk'
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        qs = Product.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            qs = qs.filter(
+                Q(price__icontains=query) |
+                Q(color__icontains=query) |
+                Q(brand__icontains=query)
+            ).distinct()
+        return qs
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
