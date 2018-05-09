1c4492249885a1e08da2f730f730b0f27f1af8e0
VinayakBagaria <workvinayak95@gmail.com>
2018-05-09 12:50:34 +0530

    colors from api populated

--- frontend/src/app/components/ColorFilter.js
+++ frontend/src/app/components/ColorFilter.js
@@ -3,40 +3,50 @@ import Select from 'react-select';
 import * as Animated from 'react-select/lib/animated';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import { colorFilter } from './../../actions';
-
-const COLOURS = [
-  { label: 'Red', value: 'Red' },
-  { label: 'Black', value: 'Black' },
-  { label: 'Blue', value: 'Blue' },
-  { label: 'Yellow', value: 'Yellow' },
-];
 
 class ColorFilter extends React.Component {
   state = {
     value: '',
+    colors: [],
   };
+
+  async componentDidMount() {
+    const colorsApi = await axios('/api/colors');
+    const colors = [];
+    colorsApi.data.map(c =>
+      colors.push({
+        label: c.color,
+        value: c.color,
+      }));
+    this.setState({ colors });
+  }
 
   render() {
     this.props.dispatch(colorFilter(this.state.value));
     return (
-      <Select
-        className="multi-select"
-        closeMenuOnSelect={false}
-        isMulti
-        components={Animated}
-        onChange={value => this.setState({ value })}
-        onMenuClose={() => {
-          if (this.state.value.length) {
-            const result = this.state.value.map(a => a.label);
-            this.props.dispatch(colorFilter(Object.values(result).toString()));
-          }
-        }}
-        options={COLOURS}
-        placeholder="Select color(s)"
-        simpleValue
-        value={this.state.value}
-      />
+      <React.Fragment>
+        {this.state.colors.length && (
+          <Select
+            className="multi-select"
+            closeMenuOnSelect={false}
+            isMulti
+            components={Animated}
+            onChange={value => this.setState({ value })}
+            onMenuClose={() => {
+              if (this.state.value.length) {
+                const result = this.state.value.map(a => a.label);
+                this.props.dispatch(colorFilter(Object.values(result).toString()));
+              }
+            }}
+            options={this.state.colors}
+            placeholder="Select color(s)"
+            simpleValue
+            value={this.state.value}
+          />
+        )}
+      </React.Fragment>
     );
   }
 }
