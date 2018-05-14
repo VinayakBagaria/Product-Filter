1e9774c00572407f833eb93c72eb837a286ad84e
VinayakBagaria <workvinayak95@gmail.com>
2018-05-15 00:07:01 +0530

    ran prettier, dont look at build

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
 const configureStore = () =>
