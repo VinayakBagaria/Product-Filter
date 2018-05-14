586ad725a24076ce7a875be944ff8976fa3b0d0a
VinayakBagaria <workvinayak95@gmail.com>
2018-05-15 01:26:46 +0530

    fixed paginator container

--- frontend/src/app/containers/Paginator.js
+++ frontend/src/app/containers/Paginator.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
-import PropTypes from 'prop-types';
 
-const Paginator = ({ totalPages, onPageChange }) => (
-  <div id="react-paginate">
-    <ReactPaginate
-      previousLabel="previous"
-      nextLabel="next"
-      breakLabel={<a href="/">...</a>}
-      breakClassName="break-me"
-      pageCount={totalPages}
-      marginPagesDisplayed={2}
-      pageRangeDisplayed={5}
-      onPageChange={onPageChange}
-      containerClassName="pagination"
-      subContainerClassName="pages pagination"
-      activeClassName="active"
-    />
-  </div>
-);
+type Props = {
+  totalPages: number,
+  onPageChange: Function,
+};
 
-Paginator.propTypes = {
-  totalPages: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
+const Paginator = (props: Props) => {
+  const { totalPages, onPageChange } = props;
+  return (
+    <div id="react-paginate">
+      <ReactPaginate
+        previousLabel="previous"
+        nextLabel="next"
+        breakLabel={<a href="/">...</a>}
+        breakClassName="break-me"
+        pageCount={totalPages}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={onPageChange}
+        containerClassName="pagination"
+        subContainerClassName="pages pagination"
+        activeClassName="active"
+      />
+    </div>
+  );
 };
 
 export default Paginator;
