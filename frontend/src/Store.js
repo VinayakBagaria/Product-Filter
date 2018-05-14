05caf6b6da8b10ed6ce8d16e90d71e38af3e2c86
VinayakBagaria <workvinayak95@gmail.com>
2018-05-14 21:16:33 +0530

    no requirement for logger middleware

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,15 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-
-const loggerMiddleware = createLogger();
 
 const configureStore = () =>
   createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware))
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
   );
 
 export default configureStore;
