5effc2295285e1a2798c6d49d83e2a9ab98f37b5
VinayakBagaria <workvinayak95@gmail.com>
2018-05-16 10:59:59 +0530

    url to path for base page render

--- backend/urls.py
+++ backend/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
